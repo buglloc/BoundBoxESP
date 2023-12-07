de9600aeeada0c8f7cbf8c9d3e1751276f36be17
Andrew Krasichkov <dev@buglloc.cc>
2023-12-07 08:34:52 +0300

    font fixes

--- src/assets/font_roboto_regular_12.c
+++ src/assets/font_roboto_regular_12.c
@@ -3,6 +3,13 @@
  * Bpp: 1
  * Opts:
  ******************************************************************************/
+#ifdef __has_include
+    #if __has_include("lvgl.h")
+        #ifndef LV_LVGL_H_INCLUDE_SIMPLE
+            #define LV_LVGL_H_INCLUDE_SIMPLE
+        #endif
+    #endif
+#endif
 
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
 #include "lvgl.h"
