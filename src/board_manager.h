33235555cea10a81c29d43f784ece2e0a41fdd35
Andrew Krasichkov <dev@buglloc.cc>
2023-12-12 10:57:40 +0700

    Added more sensors (#18)

--- src/board_manager.h
+++ src/board_manager.h
@@ -2,6 +2,7 @@
 #define BB_BOARD_MANAGER_H
 
 #include "config.h"
+#include "board_config.h"
 #include "net_manager.h"
 
 class TBoardManager
@@ -21,10 +22,22 @@ public:
   {
     BoardState State;
     IPAddress LocalIP;
+    uint32_t BattVoltage;
+    float CoreTemp;
 
     bool operator==(const BoardInfo& info) const
     {
-      return State == info.State && LocalIP == info.LocalIP;
+      return (
+        State == info.State &&
+        LocalIP == info.LocalIP &&
+      #if HAVE_BATTERY
+        abs((int)BattVoltage - (int)info.BattVoltage) < 50 &&
+      #endif
+      #if HAVE_TEMP_SENSOR
+        fabs(CoreTemp - info.CoreTemp) < 0.1 &&
+      #endif
+        true
+      );
     }
   };
 
@@ -40,6 +53,8 @@ public:
   const TConfig& RuntimeConfig() const;
   uint32_t Uptime() const;
   uint32_t FreeHeap() const;
+  uint32_t BattVoltage() const;
+  float CoreTemp() const;
 
 private:
   TBoardManager() = default;
