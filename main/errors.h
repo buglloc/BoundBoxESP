1fb6dd8558ae5326a9d898a6923c71e6765c2cb8
Andrew Krasichkov <dev@buglloc.cc>
2023-12-23 08:48:17 +0300

    assert action

--- main/errors.h
+++ main/errors.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <sdkconfig.h>
 
 #include <stdint.h>
 
@@ -19,11 +20,17 @@ enum class Error: uint8_t
   ShitHappens
 };
 
+#ifdef CONFIG_SHUTDOWN_ON_ASSERT
+  #define ASSERT_ACTION Hardware::Manager::Instance().Board().Shutdown();
+#else
+  #define ASSERT_ACTION abort();
+#endif
+
 #define ESP_SHUTDOWN_ON_ERROR(x, log_tag, format, ...) do {                                             \
   esp_err_t err_rc_ = (x);                                                                              \
   if (unlikely(err_rc_ != ESP_OK)) {                                                                    \
     ESP_LOGE(log_tag, "[%d] %s(%d): " format, (int)err_rc_, __FUNCTION__, __LINE__, ##__VA_ARGS__);     \
-    Hardware::Manager::Instance().Board().Shutdown();                                                   \
+    ASSERT_ACTION                                                                                       \
   }                                                                                                     \
 } while(0)
 
@@ -31,13 +38,13 @@ enum class Error: uint8_t
   Error err_ = (x);                                                                                  \
   if (unlikely(err_ != Error::None)) {                                                               \
     ESP_LOGE(log_tag, "[%d] %s(%d): " format, (int)err_, __FUNCTION__, __LINE__, ##__VA_ARGS__);     \
-    Hardware::Manager::Instance().Board().Shutdown();                                                \
+    ASSERT_ACTION                                                                                    \
   }                                                                                                  \
 } while(0)
 
 #define HALT_ASSERT(x, log_tag, format, ...) do {                                       \
   if (unlikely(!(x))) {                                                                 \
     ESP_LOGE(log_tag, "%s(%d): " format, __FUNCTION__, __LINE__, ##__VA_ARGS__);        \
-    Hardware::Manager::Instance().Board().Shutdown();                                   \
+    ASSERT_ACTION                                                                       \
   }                                                                                     \
 } while(0)
