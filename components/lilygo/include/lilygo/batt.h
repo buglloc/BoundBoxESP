f262af6c086870556b9b99c09567080e3fa1e86d
Andrew Krasichkov <dev@cc.buglloc.cc>
2024-01-07 15:26:05 +0300

    Battery support

--- components/lilygo/include/lilygo/batt.h
+++ components/lilygo/include/lilygo/batt.h
@@ -1,14 +1,8 @@
 #pragma once
 
-#include <driver/temperature_sensor.h>
+#include <esp_adc/adc_oneshot.h>
 
 #include <esp_err.h>
-
-// fwd
-struct adc_oneshot_unit_ctx_t;
-typedef struct adc_oneshot_unit_ctx_t *adc_oneshot_unit_handle_t;
-struct adc_cali_scheme_t;
-typedef struct adc_cali_scheme_t *adc_cali_handle_t;
 
 
 namespace LilyGo
@@ -22,6 +16,8 @@ namespace LilyGo
     uint32_t BattVoltage();
 
   private:
+    adc_unit_t adcUnit;
+    adc_channel_t adcChannel;
     adc_oneshot_unit_handle_t adcHandle = nullptr;
     adc_cali_handle_t caliHandle = nullptr;
     bool doCalibration = false;
